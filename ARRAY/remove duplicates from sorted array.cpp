dcc37f93b59c2f1371b871a2d4fb4da074902c04
vishal0982 <vishalverma212156@gmail.com>
2026-02-03 01:28:31 +0530

    add leetcode 26 solution

---
+++ ARRAY/remove duplicates from sorted array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int index = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i - 1] < nums[i]) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
+A
