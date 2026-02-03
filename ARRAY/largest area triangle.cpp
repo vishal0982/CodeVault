5b8a35206dbe6a275eb8d9a8cbd8a6ff18101f2d
vishal0982 <vishalverma212156@gmail.com>
2026-02-04 02:50:09 +0530

    add leetcode 812 solution

---
+++ ARRAY/largest area triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& p) {
+        double ans = 0;
+
+        for (int i = 0; i < p.size(); i++) {
+            for (int j = i + 1; j < p.size(); j++) {
+                for (int k = j + 1; k < p.size(); k++) {
+                    double area = abs(
+                        p[i][0] * (p[j][1] - p[k][1]) +
+                        p[j][0] * (p[k][1] - p[i][1]) +
+                        p[k][0] * (p[i][1] - p[j][1])
+                    ) / 2.0;
+
+                    ans = max(ans, area);
+                }
+            }
+        }
+        return ans;
+    }
+};
