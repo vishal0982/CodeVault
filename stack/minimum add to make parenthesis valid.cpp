969d84109756b54a36db36abe3a438e92aef1ff0
vishal0982 <vishalverma212156@gmail.com>
2026-02-09 00:48:13 +0530

    Create minimum add to make parenthesis valid.cpp

---
+++ stack/minimum add to make parenthesis valid.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+      stack<char>st;
+      int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+
+            }
+            else {
+            if(!st.empty())st.pop();
+            else count++;
+
+        }}
+        return st.size()+count;
+        
+    }
+};
