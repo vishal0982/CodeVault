ac27fcd438f75b5df5472882b5f3c55002c9d6df
vishal0982 <vishalverma212156@gmail.com>
2026-02-09 00:37:34 +0530

    Create defanging an ip address.cpp

---
+++ String/defanging an ip address.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans = "";
+        for(char ch : address){
+            if(ch == '.')
+                ans += "[.]";
+            else
+                ans += ch;
+        }
+        return ans;
+    }
+};
